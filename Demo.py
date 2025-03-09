2e058bba88d70aeb63ffcae3421725269d607b8a
Yogeshghughe1 <86541335+Yogeshghughe1@users.noreply.github.com>
2025-03-09 15:30:35 +0530

    Python College

---
+++ Demo.py
@@ -0,0 +1,7 @@
+print("welcome")
+print("wel\ncome")
+print("welcome\thello")
+a=10
+b=20
+sum=a+b
+print(sum)
---
+++ demo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class demo
+{
+    public static void main(String args[])
+    {
+        Scanner S=new Scanner(System.in);
+        int a,b,sum;
+        System.out.print("enter 2 no");
+        a=S.nextInt();
+        b=S.nextInt();
+        sum=a+b;
+        System.out.println("Addition ="+sum);
+    }
+}
